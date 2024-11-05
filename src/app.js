faf6326a5c56adeff01a37133448bc9e13ec10c3
Kjartan Heimisson <kheimisson@gmail.com>
2024-11-05 13:54:07 +0000

    added CRUD actions

--- src/app.js
+++ src/app.js
@@ -4,9 +4,35 @@ const Restaurant = require("../models/index")
 const db = require("../db/connection");
 
 //TODO: Create your GET Request Route Below: 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 app.get("/restaurants", async (req, res) => {
     const restaurants = await Restaurant.findAll({});
     res.json(restaurants);
 })
+
+app.get('/restaurants/:id', async (req, res) => {
+    const id = req.params.id;
+    const restaurant = await Restaurant.findByPk(id);
+    res.json(restaurant);
+})
+
+app.post('/restaurants', async (req, res) => {
+    const restaurant = await Restaurant.create(req.body);
+    res.json(restaurant);
+})
+
+app.put('/restaurants/:id', async (req, res) => {
+    const updatedRestaurant = await Restaurant.update(req.body, {where: {id: req.params.id}});
+    res.json(updatedRestaurant);
+})
+
+app.delete('/restaurants/:id', async (req, res) => {
+    const deletedRestaurant = await Restaurant.destroy({where: {id: req.params.id}});
+    res.json(deletedRestaurant);
+})
+
+
 
 module.exports = app;
